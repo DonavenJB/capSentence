b8ac710105e868a3ec00a67d5b32bcf6080df13f
DonavenJB <brucedonaven25@gamil.com>
2023-01-08 16:10:17 -0800

    USING A FOREACH LOOP, USING .MAP() AND SLICE(), USING .MAP() and .REPLACE()

---
+++ solution.js
@@ -0,0 +1,35 @@
+//USING A FOREACH LOOP
+function capSentence(text) {
+    let wordsArray = text.toLowwerCase().split(' ')
+    let capsArray = []
+
+    wordsArray.forEach(word => {
+        capsArray.push(word[0].toUpperCase() + word.slice(1))
+    });
+
+    return capsArray.join(' ')
+} 
+
+//USING .MAP() AND SLICE()
+
+function capSentence(text) {
+    let wordsArray = text.toLowwerCase().split(' ')
+    let capsArray = wordsArray.map(word => {
+        return word[0].toUpperCase() + word.slice(1)
+    })
+
+    return capsArray.join(' ')
+}
+
+//USING .MAP() and .REPLACE()
+
+function capSentence(text) {
+    let wordsArray = text.toLowwerCase().split(' ')
+
+    let capsArray = wordsArray.map( word => {
+        return word.replace(word[0], word[0].toUpperCase())
+    })
+
+    return capsArray.join(' ')
+}
+
